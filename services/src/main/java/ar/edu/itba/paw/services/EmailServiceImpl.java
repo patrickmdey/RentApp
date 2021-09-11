05ecc312021d6093e9257bea3a1186137a772b4c
patrickmdey <52257842+patrickmdey@users.noreply.github.com>
2021-09-11 18:31:47 -0300

    fix: intial email configuration

--- services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -4,12 +4,9 @@ import ar.edu.itba.paw.interfaces.EmailService;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
@@ -22,36 +19,12 @@ public class EmailServiceImpl implements EmailService {
     }
 
     @Override
-    public void sendMessage(String recipient, String title, String body) {
-        Properties props = System.getProperties();
-        props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "pyd.com.ar");
-        props.put("mail.smtp.port", "465");
-
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("paw_itba@pyd.com.ar","PAW_root_123");
-            }
-        });
-
-        session.setDebug(true);
-
-        try {
-
-            Message msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress("paw_itba@pyd.com.ar", false));
-            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-            msg.setSubject(title);
-            msg.setText(body);
-            msg.setSentDate(new Date());
-
-            Transport.send(msg);
-        }
-        catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
+    public void sendMessage(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@rentapp.paw.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        emailSender.send(message);
     }
 }
---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/EmailConfig.java
@@ -0,0 +1,33 @@
+package ar.edu.itba.paw.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@ComponentScan({"ar.edu.itba.paw.webapp.controller", "ar.edu.itba.paw.services"})
+public class EmailConfig {
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("paw2021b3@gmail.com");
+        mailSender.setPassword("2021Pawb-3");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
