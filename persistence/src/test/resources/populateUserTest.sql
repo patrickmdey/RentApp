0ffe7d58bad8ff1de861b2dbf5b728ba7e72651d
Lucas <ldellisola@itba.edu.ar>
2021-10-05 18:16:13 +0200

    feat: added tests for UserDao

---
+++ persistence/src/test/resources/populateUserTest.sql
@@ -0,0 +1,5 @@
+
+-- Load Account
+INSERT INTO account (id,first_name,last_name,email,location,password,picture,type)
+values  (1,'Lucas','owner','lucas@mail.com',20,'password hash',null,0),
+        (2,'Carlos','renter','carlos@mail.com',20,'password hash',null,1);
