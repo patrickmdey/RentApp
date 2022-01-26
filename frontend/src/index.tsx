a678653ada928bf55693396a615fcd43312e24c4
Lucas <ldellisola@itba.edu.ar>
2022-01-26 11:10:40 +0100

    feat: added footer

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 import Header from './components/header';
 import RouteMapper from './routeMapper'
@@ -11,7 +11,6 @@ import Footer from "./components/footer";
 import "typeface-roboto";
 import './scss/app.scss'
 
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <React.StrictMode>
