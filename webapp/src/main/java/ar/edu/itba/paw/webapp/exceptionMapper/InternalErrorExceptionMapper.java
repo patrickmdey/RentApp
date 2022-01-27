72d790fe3de181af882eb56ef918b4a8b6474ee3
srosati <srosati@itba.edu.ar>
2022-01-27 18:19:26 -0300

    feat: i18n exceptions

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptionMapper/InternalErrorExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptionMapper/InternalErrorExceptionMapper.java
@@ -1,15 +1,19 @@
 package ar.edu.itba.paw.webapp.exceptionMapper;
 
 import ar.edu.itba.paw.models.exceptions.InternalErrorException;
+import org.glassfish.jersey.server.ContainerRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.List;
+import java.util.Locale;
 
 @Component
 @Provider
@@ -18,10 +22,15 @@ public class InternalErrorExceptionMapper implements ExceptionMapper<InternalErr
     @Autowired
     private MessageSource messageSource;
 
+    @Inject
+    private javax.inject.Provider<ContainerRequest> requestProvider;
+
     @Override
     public Response toResponse(InternalErrorException e) {
+        List<Locale> languages = requestProvider.get().getAcceptableLanguages();
+        Locale lang = languages.isEmpty() ? LocaleContextHolder.getLocale() : languages.get(0);
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.TEXT_PLAIN_TYPE).
-                entity(messageSource.getMessage(e.getMessage(), null, LocaleContextHolder.getLocale())).build();
+                entity(messageSource.getMessage(e.getMessage(), null, lang)).build();
     }
 }
 
