722c25e833f1e1b10456b3b8393d3bc0ee4af1da
Lucas <ldellisola@itba.edu.ar>
2022-01-27 20:51:13 +0100

    feat: FilterCard with form

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,39 +1,39 @@
-import { useListArticles } from "../features/api/articles/articlesSlice";
+import {useListArticles} from "../features/api/articles/articlesSlice";
 import ArticleCardList from "../components/ArticleCardList";
 import FilterCard from "../components/FilterCard";
-import { Container, Row, Col } from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 function Home() {
-  const { data, error, isLoading } = useListArticles({});
+    const {data, error, isLoading} = useListArticles({});
 
-  return (
-    <Container>
-      <Row className="align-items-start justify-content-center">
-        <Col md={3} lg={3}>
-          <FilterCard />
-        </Col>
-        <Col md={9} lg={9}>
-          {error ? (
-            <>Oh no, there was an error</>
-          ) : isLoading ? (
-            <>Loading</>
-          ) : (
-            data &&
-            (data.length === 0 ? (
-              <h1>No hay articulos</h1>
-            ) : (
-              <ArticleCardList
-                articles={data}
-                maxPage={1} //TODO: Harcoded
-                articlesPerRow={3}
-              ></ArticleCardList>
-              //data.map((article, index) => <ArticleCard key={index} {...article} />)
-            ))
-          )}
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+            <Row className="align-items-start justify-content-center">
+                <Col md={3} lg={3}>
+                    <FilterCard onUpdate={(data) => console.log(data)}/>
+                </Col>
+                <Col md={9} lg={9}>
+                    {error ? (
+                        <>Oh no, there was an error</>
+                    ) : isLoading ? (
+                        <>Loading</>
+                    ) : (
+                        data &&
+                        (data.length === 0 ? (
+                            <h1>No hay articulos</h1>
+                        ) : (
+                            <ArticleCardList
+                                articles={data}
+                                maxPage={1} //TODO: Harcoded
+                                articlesPerRow={3}
+                            ></ArticleCardList>
+                            //data.map((article, index) => <ArticleCard key={index} {...article} />)
+                        ))
+                    )}
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Home;
