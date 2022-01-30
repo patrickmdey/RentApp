92de177d9d35508ce1a3e62095092b74636f77a6
srosati <srosati@itba.edu.ar>
2022-01-30 14:03:26 -0300

    feat: categorie cards styling

--- frontend/src/components/CategoriesList.tsx
+++ frontend/src/components/CategoriesList.tsx
@@ -1,22 +1,24 @@
 import { useListCategories } from '../features/api/categories/categoriesSlice';
 import CategoryCard from './CategoryCard';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { strings } from '../i18n/i18n';
 
 export default function CategoriesList() {
 	const { data, isSuccess } = useListCategories();
 	return (
-		<div className='bg-color-secondary w-100 pt-3'>
-			<h3 className='text-center'>{strings.collection.categories.searchByCategories}</h3>
-			<Row md={data ? data.length : 1} className='landing-category-container'>
-				{isSuccess &&
-					data &&
-					data.map((cat) => (
-						<Col key={cat.url.toString()}>
-							<CategoryCard key={cat.url.toString()} {...cat} />
-						</Col>
-					))}
-			</Row>
+		<div className='bg-color-secondary pt-3' style={{ width: '100vw' }}>
+			<Container>
+				<h3 className='text-center mt-0 pt-0'>{strings.collection.categories.searchByCategories}</h3>
+				<Row className='g-5 pb-4'>
+					{isSuccess &&
+						data &&
+						data.map((cat) => (
+							<Col key={cat.url.toString()}>
+								<CategoryCard key={cat.url.toString()} {...cat} />
+							</Col>
+						))}
+				</Row>
+			</Container>
 		</div>
 	);
 }
--- frontend/src/components/CategoryCard.tsx
+++ frontend/src/components/CategoryCard.tsx
@@ -9,8 +9,8 @@ interface CategoryProps {
 export default function CategoryCard(props: CategoryProps) {
 	return (
 		<LinkContainer to={props.url}>
-			<Card className='category-card h-100'>
-				<Card.Img variant='top' src={props.imageUrl.toString()} alt={props.description} />
+			<Card>
+				<Card.Img variant='top' src={props.imageUrl.toString()} alt={props.description} height='100rem' />
 				<Card.Body className='text-center'>
 					<Card.Title>{props.description}</Card.Title>
 				</Card.Body>
