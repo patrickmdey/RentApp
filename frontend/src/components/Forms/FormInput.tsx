1d868b238529a3c8cf9d11ab741da707819ac78a
srosati <srosati@itba.edu.ar>
2022-02-01 18:32:21 -0300

    refactor: changed validation parameter on FormInput from any

--- frontend/src/components/Forms/FormInput.tsx
+++ frontend/src/components/Forms/FormInput.tsx
@@ -1,40 +1,36 @@
-import { FormGroup, FormLabel, InputGroup, FormControl } from "react-bootstrap";
-import { Path, UseFormRegister } from "react-hook-form";
+import { FormGroup, FormLabel, InputGroup, FormControl } from 'react-bootstrap';
+import { Path, PathValue, UseFormRegister, Validate, ValidationRule } from 'react-hook-form';
+
+interface ValidationInterface<T> {
+	required: string | ValidationRule<boolean>;
+	min: ValidationRule<string | number>;
+	max: ValidationRule<string | number>;
+	minLength: ValidationRule<number>;
+	maxLength: ValidationRule<number>;
+	pattern: ValidationRule<RegExp>;
+	validate: Validate<PathValue<T, Path<T>>> | Record<string, Validate<PathValue<T, Path<T>>>>;
+	deps: string[];
+}
 
 export default function FormInput<T>(props: {
-  register: UseFormRegister<T>;
-  label: string;
-  name: Path<T>;
-  type: string;
-  placeholder?: string;
-  prependIcon?: JSX.Element | string | null;
-  appendIcon?: JSX.Element | null;
-  validation?: any | null; //TODO: como le paso un {} que tiene props variables adentro ?
+	register: UseFormRegister<T>;
+	label: string;
+	name: Path<T>;
+	type: string;
+	placeholder?: string;
+	prependIcon?: JSX.Element | string | null;
+	appendIcon?: JSX.Element | null;
+	validation?: Partial<ValidationInterface<T>>;
 }) {
-  const {
-    register,
-    label,
-    name,
-    type,
-    placeholder,
-    prependIcon,
-    appendIcon,
-    validation,
-  } = props;
-  return (
-    <FormGroup>
-      <FormLabel>{label}</FormLabel>
-      <InputGroup>
-        {prependIcon != null && (
-          <InputGroup.Text>{prependIcon}</InputGroup.Text>
-        )}
-        <FormControl
-          type={type}
-          placeholder={placeholder}
-          {...register(name, validation)}
-        />
-        {appendIcon != null && <InputGroup.Text>{appendIcon}</InputGroup.Text>}
-      </InputGroup>
-    </FormGroup>
-  );
+	const { register, label, name, type, placeholder, prependIcon, appendIcon, validation } = props;
+	return (
+		<FormGroup>
+			<FormLabel>{label}</FormLabel>
+			<InputGroup>
+				{prependIcon != null && <InputGroup.Text>{prependIcon}</InputGroup.Text>}
+				<FormControl type={type} placeholder={placeholder} {...register(name, validation)} />
+				{appendIcon != null && <InputGroup.Text>{appendIcon}</InputGroup.Text>}
+			</InputGroup>
+		</FormGroup>
+	);
 }
