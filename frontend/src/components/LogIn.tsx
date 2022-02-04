2c7180b322ebc32bae930d4a843a6df78fb974bb
srosati <srosati@itba.edu.ar>
2022-02-04 17:09:26 -0300

    feat: get current user id

--- frontend/src/components/LogIn.tsx
+++ frontend/src/components/LogIn.tsx
@@ -1,89 +1,91 @@
-import {Button, Card, Container, Form, FormControl, InputGroup, Row, Col, Stack} from 'react-bootstrap';
-import {strings} from '../i18n/i18n';
-import {Controller, Path, useForm} from 'react-hook-form';
-import {BsEye} from 'react-icons/bs';
-import {useEffect, useState} from 'react';
-import {LinkContainer} from 'react-router-bootstrap';
-import {useLogin} from '../features/api/authentication/authenticationSlice';
+import { Button, Card, Form, Row, Stack } from 'react-bootstrap';
+import { strings } from '../i18n/i18n';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useLogin } from '../features/api/authentication/authenticationSlice';
 import FormInput from './Forms/FormInput';
 import FormCheckbox from './Forms/FormCheckbox';
-import {useAppDispatch} from '../hooks';
-import {Action} from '@reduxjs/toolkit';
-import {setCredentials} from '../features/auth/authSlice';
+import { useAppDispatch } from '../hooks';
+import { setCredentials } from '../features/auth/authSlice';
 
 interface LogInForm {
-    email: string;
-    password: string;
-    rememberMe: boolean;
+	email: string;
+	password: string;
+	rememberMe: boolean;
 }
 
 export default function LogInComponent() {
-    const [state, setState] = useState({showPassword: false});
-    const [login, result] = useLogin();
-    const dispatch = useAppDispatch();
+	const [state, setState] = useState({ showPassword: false });
+	const [login, result] = useLogin();
 
-    if (result && result.data) {
-        dispatch(setCredentials({token: result.data}));
-    }
+	const dispatch = useAppDispatch();
 
-    function updatePasswordType() {
-        state.showPassword = !state.showPassword;
-    }
+	if (result && result.data) {
+		dispatch(setCredentials({ token: result.data }));
+	}
 
-    const {register, handleSubmit, formState: {errors}} = useForm<LogInForm>({
-        defaultValues: {email: '', password: ''}
-    });
+	function updatePasswordType() {
+		state.showPassword = !state.showPassword;
+	}
 
-    function onSubmit(data: LogInForm) {
-        const d = login(data);
-    }
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<LogInForm>({
+		defaultValues: { email: '', password: '' }
+	});
 
-    return (
-        <Card className='shadow card-style create-card mx-3'>
-            <Card.Body className='form-container'>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <h3 className='fw-bold my-1'>{strings.collection.login.title}</h3>
-                    <hr/>
-                    <Row xs={1} className='g-2'>
-                        <FormInput
-                            register={register}
-                            type='email'
-                            name='email'
-                            label={strings.collection.login.email}
-                            placeholder={strings.collection.login.emailPlaceholder}
-                            error={errors.email}
-                            errorMessage={strings.collection.login.errors.email}
-                            validation={{required: true, minLength: 3, maxLength: 320}}
+	function onSubmit(data: LogInForm) {
+		const d = login(data);
+	}
 
-                        />
-                        <FormInput
-                            register={register}
-                            type='password'
-                            name='password'
-                            label={strings.collection.login.password}
-                            placeholder={strings.collection.login.passwordPlaceholder}
-                            error={errors.password}
-                            errorMessage={strings.collection.login.errors.password}
-                            validation={{required: true, minLength: 8, maxLength: 20}}
-                        />
-                        <FormCheckbox
-                            register={register}
-                            label={strings.collection.login.rememberMe}
-                            name='rememberMe'
-                        />
-                        <Stack direction='vertical'>
-                            <Button type='submit' className='btn-block bg-color-action btn-dark mt-3 mb-2'>
-                                {strings.collection.login.loginButton}
-                            </Button>
-                            <LinkContainer to='/register'>
-                                <button type='button' className='color-action btn'>
-                                    {strings.collection.login.signupButton}
-                                </button>
-                            </LinkContainer>
-                        </Stack>
-                    </Row>
-                </Form>
-            </Card.Body>
-        </Card>
-    );
+	return (
+		<Card className='shadow card-style create-card mx-3'>
+			<Card.Body className='form-container'>
+				<Form onSubmit={handleSubmit(onSubmit)}>
+					<h3 className='fw-bold my-1'>{strings.collection.login.title}</h3>
+					<hr />
+					<Row xs={1} className='g-2'>
+						<FormInput
+							register={register}
+							type='email'
+							name='email'
+							label={strings.collection.login.email}
+							placeholder={strings.collection.login.emailPlaceholder}
+							error={errors.email}
+							errorMessage={strings.collection.login.errors.email}
+							validation={{ required: true, minLength: 3, maxLength: 320 }}
+						/>
+						<FormInput
+							register={register}
+							type='password'
+							name='password'
+							label={strings.collection.login.password}
+							placeholder={strings.collection.login.passwordPlaceholder}
+							error={errors.password}
+							errorMessage={strings.collection.login.errors.password}
+							validation={{ required: true, minLength: 8, maxLength: 20 }}
+						/>
+						<FormCheckbox
+							register={register}
+							label={strings.collection.login.rememberMe}
+							name='rememberMe'
+						/>
+						<Stack direction='vertical'>
+							<Button type='submit' className='btn-block bg-color-action btn-dark mt-3 mb-2'>
+								{strings.collection.login.loginButton}
+							</Button>
+							<LinkContainer to='/register'>
+								<button type='button' className='color-action btn'>
+									{strings.collection.login.signupButton}
+								</button>
+							</LinkContainer>
+						</Stack>
+					</Row>
+				</Form>
+			</Card.Body>
+		</Card>
+	);
 }
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,105 +1,83 @@
-import { useAppSelector } from "../hooks";
-import { Container, Image, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import React from "react";
-import {
-  BsBoxArrowInLeft,
-  BsFillInboxFill,
-  BsPersonFill,
-} from "react-icons/bs";
-import { Link } from "react-router-dom";
-import { strings } from "../i18n/i18n";
+import { useAppSelector } from '../hooks';
+import { Container, Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import React from 'react';
+import { BsBoxArrowInLeft, BsFillInboxFill, BsPersonFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { strings } from '../i18n/i18n';
 
-const logo = require("../images/rentapp-logo.png");
+const logo = require('../images/rentapp-logo.png');
 
 function RenderLoggedInNavBar() {
-  // const name = useAppSelector((state) => "Lucas")
-  const name = "lucas";
+	// const name = useAppSelector((state) => "Lucas")
+	const name = 'lucas';
 
-  return (
-    <NavDropdown
-      title={name}
-      className="active color-grey h6"
-      id="collasible-nav-dropdown"
-    >
-      <NavDropdown.Item>
-        <Link to="/proposals">
-          <BsFillInboxFill />
-          {strings.collection.header.requests}
-        </Link>
-      </NavDropdown.Item>
+	return (
+		<NavDropdown title={name} className='active color-grey h6' id='collasible-nav-dropdown'>
+			<NavDropdown.Item>
+				<Link to='/proposals'>
+					<BsFillInboxFill />
+					{strings.collection.header.requests}
+				</Link>
+			</NavDropdown.Item>
 
-      <NavDropdown.Item>
-        <Link to="/saDASD">
-          <BsPersonFill />
-          {strings.collection.header.profile}
-        </Link>
-      </NavDropdown.Item>
+			<NavDropdown.Item>
+				<Link to='/saDASD'>
+					<BsPersonFill />
+					{strings.collection.header.profile}
+				</Link>
+			</NavDropdown.Item>
 
-      <NavDropdown.Item>
-        <Link to="/ASDASDSAD">
-          <BsBoxArrowInLeft />
-          {strings.collection.header.logout}
-        </Link>
-      </NavDropdown.Item>
-    </NavDropdown>
-  );
+			<NavDropdown.Item>
+				<Link to='/ASDASDSAD'>
+					<BsBoxArrowInLeft />
+					{strings.collection.header.logout}
+				</Link>
+			</NavDropdown.Item>
+		</NavDropdown>
+	);
 }
 
 function RenderLoggedOutNavBar() {
-  return (
-    <React.Fragment>
-      <LinkContainer to="/login">
-        <Nav.Link className="active h6">
-          {strings.collection.header.login}
-        </Nav.Link>
-      </LinkContainer>
+	return (
+		<React.Fragment>
+			<LinkContainer to='/login'>
+				<Nav.Link className='active h6'>{strings.collection.header.login}</Nav.Link>
+			</LinkContainer>
 
-      <LinkContainer to="/register">
-        <Nav.Link className="active h6">
-          {strings.collection.header.signup}
-        </Nav.Link>
-      </LinkContainer>
-    </React.Fragment>
-  );
+			<LinkContainer to='/register'>
+				<Nav.Link className='active h6'>{strings.collection.header.signup}</Nav.Link>
+			</LinkContainer>
+		</React.Fragment>
+	);
 }
 
 export default function Header() {
-  const isAuthenticated = useAppSelector((state) => state.auth.token != null);
+	const isAuthenticated = useAppSelector((state) => state.auth.token != null);
 
-  return (
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      bg="primary"
-      className="mb-2"
-      variant="dark"
-    >
-      <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand>
-            <Image alt="rentapp" src={logo} height="50px" />
-          </Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className="mt-2" id="responsive-nav-bar">
-          <Nav className="ms-auto">
-            <LinkContainer to="/marketplace">
-              <Nav.Link className="active h6">
-                {strings.collection.header.marketplace}
-              </Nav.Link>
-            </LinkContainer>
+	return (
+		<Navbar collapseOnSelect expand='lg' bg='primary' className='mb-2' variant='dark'>
+			<Container>
+				<LinkContainer to='/'>
+					<Navbar.Brand>
+						<Image alt='rentapp' src={logo} height='50px' />
+					</Navbar.Brand>
+				</LinkContainer>
+				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse className='mt-2' id='responsive-nav-bar'>
+					<Nav className='ms-auto'>
+						<LinkContainer to='/marketplace'>
+							<Nav.Link className='active h6'>{strings.collection.header.marketplace}</Nav.Link>
+						</LinkContainer>
 
-            <LinkContainer to="/createArticle">
-              <Nav.Link className="active h6">
-                {strings.collection.header.publishArticle}
-              </Nav.Link>
-            </LinkContainer>
+						<LinkContainer to='/createArticle'>
+							<Nav.Link className='active h6'>{strings.collection.header.publishArticle}</Nav.Link>
+						</LinkContainer>
 
-            {isAuthenticated ? RenderLoggedInNavBar() : RenderLoggedOutNavBar()}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+						{isAuthenticated ? RenderLoggedInNavBar() : RenderLoggedOutNavBar()}
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
