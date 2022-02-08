4d5ae33d8be314d64c8cee1350e3ac1c1bf1a385
matiaslombardi <mflombardi2000@gmail.com>
2022-02-08 17:12:01 -0300

    Fix: LocalDate validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/annotations/FutureDateValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/annotations/FutureDateValidator.java
@@ -6,22 +6,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-public class FutureDateValidator implements ConstraintValidator<FutureDate, String> {
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+public class FutureDateValidator implements ConstraintValidator<FutureDate, LocalDate> {
+    //private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public void initialize(FutureDate futureDate) {}
 
     @Override
-    public boolean isValid(String dateStr, ConstraintValidatorContext constraintValidatorContext) {
-        LocalDate date;
-
-        try {
-            date = LocalDate.parse(dateStr, DATE_FORMAT);
-        } catch (DateTimeParseException e) {
-            return false;
-        }
-
+    public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
         LocalDate today = LocalDate.now();
 
         return date.equals(today) || date.isAfter(today);
