fbbf064e52f0a4feec37a030b7b6f0d4a7dd0d6b
srosati <srosati@itba.edu.ar>
2022-02-09 14:57:32 -0300

    fix: Read rating from component

--- frontend/src/pages/Review.tsx
+++ frontend/src/pages/Review.tsx
@@ -1,26 +1,24 @@
-import { Helmet } from "react-helmet";
-import { useFindArticle } from "../features/api/articles/articlesSlice";
-import { Container } from "react-bootstrap";
-import ReviewForm from "../components/Review/ReviewForm";
+import { Helmet } from 'react-helmet';
+import { useFindArticle } from '../features/api/articles/articlesSlice';
+import { Container } from 'react-bootstrap';
+import ReviewForm from '../components/Review/ReviewForm';
 function Review() {
-  //TODO: getSearchParams() pero ver como es con router .v6
+	//TODO: getSearchParams() pero ver como es con router .v6
 
-  const { data: article, isSuccess } = useFindArticle(
-    new URL("http://localhost:8080/articles/63")
-  );
+	const { data: article, isSuccess } = useFindArticle(new URL('articles/44', process.env.REACT_APP_BASE_URL));
 
-  return (
-    <>
-      <Helmet>
-        <title>Create Review</title>
-      </Helmet>
-      {isSuccess && article && (
-        <Container style={{ margin: 20 % 0 }} className="mx-auto min-height">
-          <ReviewForm article={article} />
-        </Container>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Helmet>
+				<title>Create Review</title>
+			</Helmet>
+			{isSuccess && article && (
+				<Container style={{ margin: 20 % 0 }} className='mx-auto min-height'>
+					<ReviewForm article={article} />
+				</Container>
+			)}
+		</>
+	);
 }
 
 export default Review;
