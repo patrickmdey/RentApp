071f7efe15b8ac395d1381625b666878b8e3fade
srosati <srosati@itba.edu.ar>
2022-02-10 19:30:25 -0300

    feat: multiple files upload on createArticle

--- frontend/src/components/Forms/ImageInput.tsx
+++ frontend/src/components/Forms/ImageInput.tsx
@@ -1,29 +1,31 @@
-import React, { useRef } from 'react';
-import { FormGroup, FormLabel, InputGroup, FormControl, Form } from 'react-bootstrap';
-import { UseFormRegister, FieldError, Path } from 'react-hook-form';
-import ValidationInterface from './ValidationInterface';
+import { useRef } from 'react';
+import { Button, FormGroup, FormLabel, InputGroup } from 'react-bootstrap';
 
-export default function ImageInput<T>(props: {
-	register: UseFormRegister<T>;
-	name: Path<T>;
-	label?: string;
-	onSelect: Function;
-}) {
-	const { register, label, name, onSelect } = props;
+export default function ImageInput<T>(props: { label?: string; onSelect: Function }) {
+	const { label, onSelect } = props;
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	return (
 		<FormGroup>
 			{label && <FormLabel>{label}</FormLabel>}
 			<InputGroup>
-				<FormControl
+				<Button
+					style={{ width: '40px', height: '40px', borderRadius: 0 }}
+					variant='outline-secondary'
+					onClick={() => inputRef.current?.click()}
+				>
+					+
+				</Button>
+				<input
 					type='file'
-					{...register(name)}
+					className='d-none'
 					ref={inputRef}
 					onChange={(_) => {
-						console.log(inputRef?.current?.files);
-						if (inputRef?.current?.files && inputRef?.current?.files.length)
-							onSelect(inputRef?.current?.files[0]);
+						if (inputRef?.current?.files && inputRef?.current?.files.length) {
+							onSelect(inputRef.current.files[0]);
+							inputRef.current.files = null;
+							inputRef.current.value = '';
+						}
 					}}
 				/>
 			</InputGroup>
