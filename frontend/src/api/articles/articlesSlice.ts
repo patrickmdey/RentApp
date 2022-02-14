5e312024504f3715181990ed4c4cad458619bcd7
srosati <srosati@itba.edu.ar>
2022-02-14 17:58:27 -0300

    fix: cache paginated response on articles

--- frontend/src/api/articles/articlesSlice.ts
+++ frontend/src/api/articles/articlesSlice.ts
@@ -7,12 +7,19 @@ const ArticlesApiSlice = BaseApiSlice.injectEndpoints({
 	endpoints: (build) => ({
 		findArticle: build.query<Article, string>({
 			query: (url) => url.toString(),
-			providesTags: (result) => (result ? [{ type: 'Article' as const, id: result.id }] : ['Article'])
+			providesTags: (result) => (result ? [{ type: 'Article', id: result.id }] : ['Article'])
 		}),
 
 		listArticles: build.query<PaginatedData<Article[]>, ListArticleParameters>({
 			query: (params) => `articles?${new URLSearchParams(Object.entries(params)).toString()}`,
-			transformResponse: (response: Article[], meta) => paginatedResponse(response, meta)
+			transformResponse: (response: Article[], meta) => paginatedResponse(response, meta),
+			providesTags: (result) =>
+				result
+					? [
+							...result.data.map(({ id }) => ({ type: 'Article' as const, id: id })),
+							{ type: 'Article', id: 'PARTIAL-LIST' }
+					  ]
+					: [{ type: 'Article', id: 'PARTIAL-LIST' }]
 		}),
 
 		listRelatedArticles: build.query<Article[], string>({
