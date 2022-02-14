48ea328e0f6858355860c584e8f2094416cf5536
srosati <srosati@itba.edu.ar>
2022-02-14 17:03:34 -0300

    fix: category cards in landing columns

--- frontend/src/components/Category/CategoriesList.tsx
+++ frontend/src/components/Category/CategoriesList.tsx
@@ -1,25 +1,27 @@
-import {useListCategories} from '../../api/categories/categoriesSlice';
+import { useListCategories } from '../../api/categories/categoriesSlice';
 import CategoryCard from './CategoryCard';
-import {Card, Col, Container, Row} from 'react-bootstrap';
-import {strings} from '../../i18n/i18n';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { strings } from '../../i18n/i18n';
 
 export default function CategoriesList() {
 	const { data, isSuccess } = useListCategories();
 	return (
-		<Card className="card-style category-card-list bg-color-secondary">
-			<Container>
-				<Card.Title as="h3"> {strings.collection.categories.searchByCategories}</Card.Title>
-				<hr/>
-				<Row md={4} lg={4} className='justify-content-center'>
-					{isSuccess &&
-						data &&
-						data.map((cat) => (
-							<Col key={cat.id}>
-								<CategoryCard {...cat} />
-							</Col>
-						))}
-				</Row>
-			</Container>
+		<Card className='p-3 bg-color-secondary card-style'>
+			<Card.Title as='h3'> {strings.collection.categories.searchByCategories}</Card.Title>
+			<hr />
+			<Card.Body>
+				<Container>
+					<Row xs={7} className='w-100'>
+						{isSuccess &&
+							data &&
+							data.map((cat) => (
+								<Col key={cat.id}>
+									<CategoryCard {...cat} />
+								</Col>
+							))}
+					</Row>
+				</Container>
+			</Card.Body>
 		</Card>
 	);
 }
