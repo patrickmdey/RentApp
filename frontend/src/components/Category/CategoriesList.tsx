2dd5f6915cd57619752f08daef4eb8c60398628e
patrickmdey <patrickmdey@hotmail.com>
2022-02-14 16:18:03 -0300

    refact: Landing.tsx

--- frontend/src/components/Category/CategoriesList.tsx
+++ frontend/src/components/Category/CategoriesList.tsx
@@ -1,24 +1,24 @@
-import { useListCategories } from '../../api/categories/categoriesSlice';
+import {useListCategories} from '../../api/categories/categoriesSlice';
 import CategoryCard from './CategoryCard';
-import { Col, Container, Row } from 'react-bootstrap';
-import { strings } from '../../i18n/i18n';
+import {Card, Col, Container, Row} from 'react-bootstrap';
+import {strings} from '../../i18n/i18n';
 
 export default function CategoriesList() {
-	const { data, isSuccess } = useListCategories();
-	return (
-		<div className='bg-color-secondary pt-3' style={{ width: '100vw' }}>
-			<Container>
-				<h3 className='text-center mt-0 pt-0'>{strings.collection.categories.searchByCategories}</h3>
-				<Row className='g-5 pb-4'>
-					{isSuccess &&
-						data &&
-						data.map((cat) => (
-							<Col key={cat.url.toString()}>
-								<CategoryCard key={cat.url.toString()} {...cat} />
-							</Col>
-						))}
-				</Row>
-			</Container>
-		</div>
-	);
+    const {data, isSuccess} = useListCategories();
+    return (
+        <Card className="card-style category-card-list bg-color-secondary">
+            <Card.Title as="h3"> {strings.collection.categories.searchByCategories}</Card.Title>
+            <hr/>
+            <Row md={4} lg={4} className='justify-content-center'>
+                {isSuccess &&
+                    data &&
+                    data.map((cat) => (
+                        <Col key={cat.url.toString()}>
+                            <CategoryCard key={cat.url.toString()} {...cat} />
+                        </Col>
+                    ))}
+            </Row>
+        </Card>
+    )
+        ;
 }
--- frontend/src/components/Category/CategoryCard.tsx
+++ frontend/src/components/Category/CategoryCard.tsx
@@ -1,20 +1,24 @@
-import { Card } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import {Card} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import {useListImages} from "../../api/images/imagesSlice";
 
 interface CategoryProps {
-	description: string;
-	url: string;
-	imageUrl: string;
+    description: string;
+    url: string;
+    imageUrl: string;
 }
+
 export default function CategoryCard(props: CategoryProps) {
-	return (
-		<LinkContainer to={props.url}>
-			<Card>
-				<Card.Img variant='top' src={props.imageUrl} alt={props.description} height='100rem' />
-				<Card.Body className='text-center'>
-					<Card.Title>{props.description}</Card.Title>
-				</Card.Body>
-			</Card>
-		</LinkContainer>
-	);
+
+
+    return (
+        <LinkContainer to={props.url}>
+            <Card className="card-style w-50">
+                <Card.Img variant='top' src={props.imageUrl} alt={props.description} height='100rem'/>
+                <Card.Body className='d-flex text-center justify-content-center align-items-center'>
+                    <Card.Title as="h4">{props.description}</Card.Title>
+                </Card.Body>
+            </Card>
+        </LinkContainer>
+    );
 }
