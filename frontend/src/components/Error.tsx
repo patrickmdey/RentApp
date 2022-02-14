278c5ae41ce22dca7bd198a175bb93ada047cbef
patrickmdey <patrickmdey@hotmail.com>
2022-02-14 15:02:32 -0300

    feat: 404 not found view

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,16 +1,19 @@
 import errorImage from '../assets/images/error.png';
+import {strings} from "../i18n/i18n";
+
 function Error(props: {
-	error: number | 'CUSTOM_ERROR' | 'FETCH_ERROR' | 'PARSING_ERROR';
-	message: string | undefined;
+    error: number | 'CUSTOM_ERROR' | 'FETCH_ERROR' | 'PARSING_ERROR';
+    message: string | undefined;
 }) {
-	const { error, message } = props;
-	return (
-		<div className='d-flex flex-column justify-content-center align-items-center'>
-			<img src={errorImage} />
-			<h3 className='fw-bold'>Error {error}</h3>
-			<p className='lead'>{message}</p>
-		</div>
-	);
+    const {error, message} = props;
+    return (
+        <div className='d-flex flex-column justify-content-center align-items-center'>
+            <img src={errorImage}/>
+            <h3 className='fw-bold'>Error {error}</h3>
+            <p className='lead'>{message}</p>
+            <a href="/">{strings.collection.errors.pageNotFoundSubtitle}</a>
+        </div>
+    );
 }
 
 export default Error;
