cd009e1b6a603394908df11f22f89262c00a15b8
srosati <srosati@itba.edu.ar>
2022-02-18 14:21:24 -0300

    fix: delete image icon

--- frontend/src/components/FormInputs/ImagePreview.tsx
+++ frontend/src/components/FormInputs/ImagePreview.tsx
@@ -1,22 +1,22 @@
-import {Button, Image} from 'react-bootstrap';
-import {CircleFill} from "react-bootstrap-icons";
+import { Button, Image } from 'react-bootstrap';
+import { CircleFill, XCircle, XCircleFill } from 'react-bootstrap-icons';
 
 export default function ImagePreview(props: { img: string; onDelete: Function }) {
-    const {img, onDelete} = props;
-    return (
-        <div style={{position: 'relative'}}>
-            <Button
-                variant='link'
-                className='text-danger fs-4 p-0 m-0 text-decoration-none'
-                style={{position: 'absolute', top: -15, right: -5, height: '5px'}}
-                onClick={(e) => {
-                    e.preventDefault();
-                    onDelete();
-                }}
-            >
-                <CircleFill className="bg-color-rentapp-red"/>
-            </Button>
-            <Image src={img} width={40} height={40}/>
-        </div>
-    );
+	const { img, onDelete } = props;
+	return (
+		<div style={{ position: 'relative' }}>
+			<Button
+				variant='link'
+				className='text-danger fs-6 p-0 m-0 text-decoration-none'
+				style={{ position: 'absolute', top: -10, right: -10, height: '5px' }}
+				onClick={(e) => {
+					e.preventDefault();
+					onDelete();
+				}}
+			>
+				<XCircleFill className='color-rentapp-red' />
+			</Button>
+			<Image src={img} width={40} height={40} />
+		</div>
+	);
 }
