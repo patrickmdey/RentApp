b74c27d375a4edd45bd25ad63fa31d54caf92425
srosati <srosati@itba.edu.ar>
2022-02-18 15:46:59 -0300

    fix: notification badge

--- frontend/src/components/Requests/SentRequestList.tsx
+++ frontend/src/components/Requests/SentRequestList.tsx
@@ -1,10 +1,10 @@
-import {skipToken} from '@reduxjs/toolkit/dist/query';
-import {useState} from 'react';
-import {Card, Tabs, Tab, Badge} from 'react-bootstrap';
-import {useListRentProposals} from '../../api/rentProposals/rentProposalsSlice';
+import { skipToken } from '@reduxjs/toolkit/dist/query';
+import { useState } from 'react';
+import { Card, Tabs, Tab, Badge } from 'react-bootstrap';
+import { useListRentProposals } from '../../api/rentProposals/rentProposalsSlice';
 import useUserId from '../../hooks/useUserId';
-import {strings} from '../../i18n/i18n';
-import {SENT_STRING, states} from '../../views/Requests';
+import { strings } from '../../i18n/i18n';
+import { SENT_STRING, states } from '../../views/Requests';
 import NoDataCard from '../NoDataCard';
 import RequestCardList from './RequestCardList';
 import usePaginatedResponse from '../../hooks/usePaginatedResponse';
@@ -12,137 +12,151 @@ import PagesList from '../PagesList';
 import LoadingComponent from '../LoadingComponent';
 import Error from '../Error';
 
-function SentRequestList(props: { acceptedAmount: number }) {
-    const id = useUserId();
+function SentRequestList(props: { acceptedAmount: number; declinedAmount: number }) {
+	const id = useUserId();
 
-    const [key, setKey] = useState('pending');
+	const [key, setKey] = useState('pending');
 
-    const [pendingPage, setPendingPage] = useState(1);
-    const [acceptedPage, setAcceptedPage] = useState(1);
-    const [declinedPage, setDeclinedPage] = useState(1);
+	const [pendingPage, setPendingPage] = useState(1);
+	const [acceptedPage, setAcceptedPage] = useState(1);
+	const [declinedPage, setDeclinedPage] = useState(1);
 
-    const {
-        data: pendingS,
-        isSuccess: pendingSSucc,
-        pages: pendingPages,
-        isLoading: pSLoad,
-        error: pendingSError
-    } = usePaginatedResponse(
-        useListRentProposals(
-            id !== null
-                ? {
-                    userId: id,
-                    type: SENT_STRING,
-                    state: states.pending,
-                    page: pendingPage
-                }
-                : skipToken
-        )
-    );
+	const {
+		data: pendingS,
+		isSuccess: pendingSSucc,
+		pages: pendingPages,
+		isLoading: pSLoad,
+		error: pendingSError
+	} = usePaginatedResponse(
+		useListRentProposals(
+			id !== null
+				? {
+						userId: id,
+						type: SENT_STRING,
+						state: states.pending,
+						page: pendingPage
+				  }
+				: skipToken
+		)
+	);
 
-    const {
-        data: acceptedS,
-        isSuccess: acceptedSSucc,
-        pages: acceptedPages,
-        isLoading: aSLoad,
-        error: acceptedSError
-    } = usePaginatedResponse(
-        useListRentProposals(
-            id !== null
-                ? {
-                    userId: id,
-                    type: SENT_STRING,
-                    state: states.accepted,
-                    page: acceptedPage
-                }
-                : skipToken
-        )
-    );
+	const {
+		data: acceptedS,
+		isSuccess: acceptedSSucc,
+		pages: acceptedPages,
+		isLoading: aSLoad,
+		error: acceptedSError
+	} = usePaginatedResponse(
+		useListRentProposals(
+			id !== null
+				? {
+						userId: id,
+						type: SENT_STRING,
+						state: states.accepted,
+						page: acceptedPage
+				  }
+				: skipToken
+		)
+	);
 
-    const {
-        data: declinedS,
-        isSuccess: declinedSSucc,
-        pages: declinedPages,
-        isLoading: dSLoad,
-        error: declinedSError
-    } = usePaginatedResponse(
-        useListRentProposals(
-            id !== null
-                ? {
-                    userId: id,
-                    type: SENT_STRING,
-                    state: states.declined,
-                    page: declinedPage
-                }
-                : skipToken
-        )
-    );
+	const {
+		data: declinedS,
+		isSuccess: declinedSSucc,
+		pages: declinedPages,
+		isLoading: dSLoad,
+		error: declinedSError
+	} = usePaginatedResponse(
+		useListRentProposals(
+			id !== null
+				? {
+						userId: id,
+						type: SENT_STRING,
+						state: states.declined,
+						page: declinedPage
+				  }
+				: skipToken
+		)
+	);
 
-    const anyError = pendingSError || acceptedSError || declinedSError;
-    if (anyError && 'originalStatus' in anyError)
-        return (
-            <Error
-                error={anyError.originalStatus}
-                message={anyError.data}
-            />
-        );
+	const anyError = pendingSError || acceptedSError || declinedSError;
+	if (anyError && 'originalStatus' in anyError)
+		return <Error error={anyError.originalStatus} message={anyError.data} />;
 
-    return (
-        <Card className='card-style min-height' color='red'>
-            <Tabs activeKey={key} onSelect={(k) => k != null && setKey(k)}>
-                <Tab eventKey='pending' title={strings.collection.requests.pendingTitle}>
-                    {pSLoad ? (
-                        <LoadingComponent/>
-                    ) : pendingSSucc && pendingS && pendingS.length > 0 ? (
-                        <>
-                            <RequestCardList isSent={true} requests={pendingS}/>
-                            <PagesList pages={pendingPages} page={pendingPage} setPage={setPendingPage}/>
-                        </>
-                    ) : (
-                        <NoDataCard
-                            title={strings.collection.requests.sent.noPendingTitle}
-                            subtitle={strings.collection.requests.sent.noPendingSubtitle}
-                        />
-                    )}
-                </Tab>
-                <Tab eventKey='accepted' title={<div className="d-flex align-items-center">
-                    <p>{strings.collection.requests.acceptedTitle}</p>{props.acceptedAmount > 0 && (
-                    <p className='my-1'>
-                        <Badge className='bg-rentapp-red ms-1'>{props.acceptedAmount}</Badge>
-                    </p>
-                )}</div>}>
-                    {aSLoad ? (
-                        <LoadingComponent/>
-                    ) : acceptedSSucc && acceptedS && acceptedS.length > 0 ? (
-                        <>
-                            <RequestCardList isSent={true} requests={acceptedS}/>
-                            <PagesList pages={acceptedPages} page={acceptedPage} setPage={setAcceptedPage}/>
-                        </>
-                    ) : (
-                        <NoDataCard
-                            title={strings.collection.requests.sent.noAcceptedTitle}
-                            subtitle={strings.collection.requests.sent.noAcceptedSubtitle}
-                        />
-                    )}
-                </Tab>
-                <Tab eventKey='declined' title={strings.collection.requests.declinedTitle}>
-                    {dSLoad ? (
-                        <LoadingComponent/>
-                    ) : declinedSSucc && declinedS && declinedS.length > 0 ? (
-                        <>
-                            <RequestCardList isSent={true} requests={declinedS}/>
-                            <PagesList pages={declinedPages} page={declinedPage} setPage={setDeclinedPage}/>
-                        </>
-                    ) : (
-                        <NoDataCard
-                            title={strings.collection.requests.sent.noDeclinedTitle}
-                            subtitle={strings.collection.requests.sent.noDeclinedSubtitle}
-                        />
-                    )}
-                </Tab>
-            </Tabs>
-        </Card>
-    );
+	return (
+		<Card className='card-style min-height' color='red'>
+			<Tabs activeKey={key} onSelect={(k) => k != null && setKey(k)}>
+				<Tab eventKey='pending' title={strings.collection.requests.pendingTitle}>
+					{pSLoad ? (
+						<LoadingComponent />
+					) : pendingSSucc && pendingS && pendingS.length > 0 ? (
+						<>
+							<RequestCardList isSent={true} requests={pendingS} />
+							<PagesList pages={pendingPages} page={pendingPage} setPage={setPendingPage} />
+						</>
+					) : (
+						<NoDataCard
+							title={strings.collection.requests.sent.noPendingTitle}
+							subtitle={strings.collection.requests.sent.noPendingSubtitle}
+						/>
+					)}
+				</Tab>
+				<Tab
+					eventKey='accepted'
+					title={
+						<div className='d-flex align-items-center'>
+							<p>{strings.collection.requests.acceptedTitle}</p>
+							{props.acceptedAmount > 0 && (
+								<p>
+									<Badge className='bg-rentapp-red ms-1'>{props.acceptedAmount}</Badge>
+								</p>
+							)}
+						</div>
+					}
+				>
+					{aSLoad ? (
+						<LoadingComponent />
+					) : acceptedSSucc && acceptedS && acceptedS.length > 0 ? (
+						<>
+							<RequestCardList isSent={true} requests={acceptedS} />
+							<PagesList pages={acceptedPages} page={acceptedPage} setPage={setAcceptedPage} />
+						</>
+					) : (
+						<NoDataCard
+							title={strings.collection.requests.sent.noAcceptedTitle}
+							subtitle={strings.collection.requests.sent.noAcceptedSubtitle}
+						/>
+					)}
+				</Tab>
+				<Tab
+					eventKey='declined'
+					title={
+						<div className='d-flex align-items-center'>
+							<p>{strings.collection.requests.declinedTitle}</p>
+							{props.declinedAmount > 0 && (
+								<p>
+									<Badge className='bg-rentapp-red ms-1'>{props.declinedAmount}</Badge>
+								</p>
+							)}
+						</div>
+					}
+				>
+					{dSLoad ? (
+						<LoadingComponent />
+					) : declinedSSucc && declinedS && declinedS.length > 0 ? (
+						<>
+							<RequestCardList isSent={true} requests={declinedS} />
+							<PagesList pages={declinedPages} page={declinedPage} setPage={setDeclinedPage} />
+						</>
+					) : (
+						<NoDataCard
+							title={strings.collection.requests.sent.noDeclinedTitle}
+							subtitle={strings.collection.requests.sent.noDeclinedSubtitle}
+						/>
+					)}
+				</Tab>
+			</Tabs>
+		</Card>
+	);
 }
 
 export default SentRequestList;
--- frontend/src/views/Requests.tsx
+++ frontend/src/views/Requests.tsx
@@ -1,82 +1,86 @@
-import {Badge, Card, Container, Nav, Tab} from 'react-bootstrap';
-import {strings} from '../i18n/i18n';
+import { Badge, Card, Container, Nav, Tab } from 'react-bootstrap';
+import { strings } from '../i18n/i18n';
 import SentRequestList from '../components/Requests/SentRequestList';
 import ReceivedRequestList from '../components/Requests/ReceivedRequestList';
 import useUserId from '../hooks/useUserId';
-import {useFindUser} from '../api/users/usersSlice';
-import {useState, useEffect} from 'react';
+import { useFindUser } from '../api/users/usersSlice';
+import { useState, useEffect } from 'react';
 
 export const RECEIVED_STRING = 'received';
 export const SENT_STRING = 'sent';
 
 export const states = {
-    pending: 'PENDING',
-    accepted: 'ACCEPTED',
-    declined: 'DECLINED'
+	pending: 'PENDING',
+	accepted: 'ACCEPTED',
+	declined: 'DECLINED'
 };
 
 export default function Requests() {
-    const userId = useUserId();
+	const userId = useUserId();
 
-    const {data: user} = useFindUser(`users/${userId}`);
+	const { data: user } = useFindUser(`users/${userId}`);
 
-    const [receivedAmount, setReceievedAmount] = useState(0);
-    const [sentAmount, setSentAmount] = useState(0);
+	const [receivedAmount, setReceievedAmount] = useState(0);
+	const [acceptedAmount, setAcceptedAmount] = useState(0);
+	const [declinedAmount, setDeclinedAmount] = useState(0);
 
-    useEffect(() => setReceievedAmount(user && user.owner ? user.pendingRequestAmount : 0), [user]);
-    useEffect(() => setSentAmount(user ? user.acceptedRequestAmount : 0), [user]);
+	useEffect(() => setReceievedAmount(user && user.owner ? user.pendingRequestAmount : 0), [user]);
+	useEffect(() => setAcceptedAmount(user ? user.acceptedRequestAmount : 0), [user]);
+	useEffect(() => setDeclinedAmount(user ? user.declinedRequestAmount : 0), [user]);
 
-    return (
-        <Container className='min-height'>
-            <Tab.Container id='left-tabs-example' defaultActiveKey='first'>
-                <div className='row align-items-start justify-content-center mb-2 g-2'>
-                    <div className='col-md-3 col-lg-3 col-12 '>
-                        <Card className='card-style '>
-                            <Card.Body>
-                                <h4>{strings.collection.requests.title}</h4>
-                                <hr/>
-                                <Nav variant='pills' className='flex-column'>
-                                    <Nav.Item>
-                                        <Nav.Link
-                                            className='request-pill w-100 text-start mb-1 d-flex align-items-center'
-                                            eventKey='first'>
-                                            <p className='my-1'>{strings.collection.requests.sentTitle}</p>
-                                            {sentAmount > 0 && (
-                                                <p className='my-1'>
-                                                    <Badge className='bg-rentapp-red ms-1'>{sentAmount}</Badge>
-                                                </p>
-                                            )}
-                                        </Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey='second'
-                                                  className='request-pill w-100 text-start'>
-                                            <div className='d-flex align-items-center'>
-                                                <p className='my-1'>{strings.collection.requests.receivedTitle}</p>
-                                                {receivedAmount > 0 && (
-                                                    <p className='my-1'>
-                                                        <Badge className='bg-rentapp-red ms-1'>{receivedAmount}</Badge>
-                                                    </p>
-                                                )}
-                                            </div>
-                                        </Nav.Link>
-                                    </Nav.Item>
-                                </Nav>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className='col-md-9 col-lg-9 col-12 '>
-                        <Tab.Content>
-                            <Tab.Pane eventKey='first'>
-                                <SentRequestList acceptedAmount={sentAmount}/>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey='second'>
-                                <ReceivedRequestList pendingAmount={receivedAmount}/>
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </div>
-                </div>
-            </Tab.Container>
-        </Container>
-    );
+	return (
+		<Container className='min-height'>
+			<Tab.Container id='left-tabs-example' defaultActiveKey='first'>
+				<div className='row align-items-start justify-content-center mb-2 g-2'>
+					<div className='col-md-3 col-lg-3 col-12 '>
+						<Card className='card-style '>
+							<Card.Body>
+								<h4>{strings.collection.requests.title}</h4>
+								<hr />
+								<Nav variant='pills' className='flex-column'>
+									<Nav.Item>
+										<Nav.Link
+											className='request-pill w-100 text-start mb-1 d-flex align-items-center'
+											eventKey='first'
+										>
+											<p className='my-1'>{strings.collection.requests.sentTitle}</p>
+											{acceptedAmount + declinedAmount > 0 && (
+												<p className='my-1'>
+													<Badge className='bg-rentapp-red ms-1'>
+														{acceptedAmount + declinedAmount}
+													</Badge>
+												</p>
+											)}
+										</Nav.Link>
+									</Nav.Item>
+									<Nav.Item>
+										<Nav.Link eventKey='second' className='request-pill w-100 text-start'>
+											<div className='d-flex align-items-center'>
+												<p className='my-1'>{strings.collection.requests.receivedTitle}</p>
+												{receivedAmount > 0 && (
+													<p className='my-1'>
+														<Badge className='bg-rentapp-red ms-1'>{receivedAmount}</Badge>
+													</p>
+												)}
+											</div>
+										</Nav.Link>
+									</Nav.Item>
+								</Nav>
+							</Card.Body>
+						</Card>
+					</div>
+					<div className='col-md-9 col-lg-9 col-12 '>
+						<Tab.Content>
+							<Tab.Pane eventKey='first'>
+								<SentRequestList acceptedAmount={acceptedAmount} declinedAmount={declinedAmount} />
+							</Tab.Pane>
+							<Tab.Pane eventKey='second'>
+								<ReceivedRequestList pendingAmount={receivedAmount} />
+							</Tab.Pane>
+						</Tab.Content>
+					</div>
+				</div>
+			</Tab.Container>
+		</Container>
+	);
 }
